c40972a622451d2e93734a26edb0eb98b48eac97
Quinn94 <mayquinn94@gmail.com>
2020-10-15 23:26:08 +0800

    set up backend

---
+++ backend/data/products.js
@@ -0,0 +1,82 @@
+const products = [
+  {
+    _id: '1',
+    name: 'Power of Breath',
+    image: '/images/breath.jpg',
+    description:
+      'Yoga is about acknowledging the subtleties within us while deepening the connection to the self and universe. Our breath is the most powerful tool to guide our yoga practice on a deeper, more intimate level. Where the breath goes, the mind follows. When we can harness the breath, we gain power and control over the mind and body. We extend our life, vitality and greatly increase our energy, health and well-being.',
+    instructor: 'Dylan Werner',
+    category: 'Mindfulness',
+    price: 25.99,
+    slotsAvailable: 0,
+    rating: 4,
+    numReviews: 5,
+  },
+  {
+    _id: '2',
+    name: 'Psychic Sleep',
+    image: '/images/sleep-relaxation.jpg',
+    description:
+      'Psychic Sleep, also known as Yoga Nidra, is a conscious sleep where the physical body falls into the deepest state of relaxation while the mind remains fully conscious.',
+    instructor: 'Sri Dharma Mittra',
+    category: 'Mindfulness',
+    price: 19.99,
+    slotsAvailable: 10,
+    rating: 5,
+    numReviews: 1,
+  },
+  {
+    _id: '3',
+    name: 'Healing Sound Bath',
+    image: '/images/sound-bath.jpg',
+    description:
+      'Immerse yourself in the healing power of sound. Healing Sound Bath contains six sensory meditations designed to bring you to a state of deep relaxation and vibrational harmony. Inspired by ancient techniques, sound baths awaken your senses through crystal singing bowls — each one tuned to a specific chakra to clear your energy channels for full-body healing. Each meditation is guided by an intention so you can choose what you need for the day, whether it’s relief from anxiety or a daily dose of calm. Prepare to leave each session refreshed, radiant, and glowing from the inside out.',
+    instructor: 'Susy Markoe Schieffelin',
+    category: 'Mindfulness',
+    price: 23.99,
+    slotsAvailable: 0,
+    rating: 4,
+    numReviews: 6,
+  },
+  {
+    _id: '4',
+    name: 'Fluid Flow Flexibility',
+    image: '/images/flexibility.jpg',
+    description:
+      'Build flexibility and fluidity in your practice with five perfectly designed flows for each area of your body. Have fun flowing without the sets, reps, and static postures that many flexibility-building practices require. Instead, Fluid Flexibility Flows uses a series of muscle-specific flows designed to target areas of the body that we struggle the most with building flexibility in: the hamstrings, shoulders, front body, and back body.',
+    instructor: 'Briohny Smyth',
+    category: 'Yoga',
+    price: 29.99,
+    slotsAvailable: 10,
+    rating: 4,
+    numReviews: 5,
+  },
+  {
+    _id: '5',
+    name: 'Inverted Mind',
+    image: '/images/inversion.jpg',
+    description:
+      'Get a better handle onTurn your practice upside down using the power of visualization. Inverted Mind contains six challenging vinyasa classes that incorporate drills and visualization to strengthen your skills in five foundational inversions: shoulderstand, crow, headstand, forearm stand, and handstand.',
+    instructor: 'Honza Lafond',
+    category: 'Yoga',
+    price: 29.99,
+    slotsAvailable: 7,
+    rating: 5,
+    numReviews: 5,
+  },
+  {
+    _id: '6',
+    name: 'The Power of Yoga',
+    image: '/images/strength.jpg',
+    description:
+      'Sweat, strengthen, and push your limits with The Power of Yoga. You’ll explore athletic combinations of Ashtanga and Rocket-inspired sequences that push you to break through limits and explore your edge. Find heat in the strength-building fundamentals of Rocket-inspired yoga, and hone your mental focus with Ashtanga influences. You’ll challenge your strength and purify the body in these high-energy and sweat-inducing classes.',
+    instructor: 'Carson Calhoun',
+    category: 'Yoga',
+    price: 29.99,
+    slotsAvailable: 0,
+    rating: 4,
+    numReviews: 2,
+  },
+];
+
+module.exports = products;
