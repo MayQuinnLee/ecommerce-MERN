bc5fdbb44ee10a54d43052723cc811de663ee098
Quinn94 <mayquinn94@gmail.com>
2020-10-18 22:15:04 +0800

    mongoose setup with models

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,15 @@
 import express from 'express'
-import products from './data/products.js'
+import colors from 'colors'
 import dotenv from 'dotenv'
+import connectDB from './config/db.js'
+import products from './data/products.js'
 
 dotenv.config()
 
+connectDB()
+
 const app = express()
+
 
 app.get('/', (req, res) => {
   res.send('API is running...')
@@ -21,4 +26,4 @@ app.get('/api/products/:id', (req, res) => {
 
 const PORT = process.env.PORT || 5000
 
-app.listen(PORT, () => console.log(`Server is running in ${process.env.NODE_ENV} mode on port ${PORT}`))
+app.listen(PORT, () => console.log(`Server is running in ${process.env.NODE_ENV} mode on port ${PORT}`.blue.bold))
