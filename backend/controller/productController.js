11eaed25609fc20450fb6f3d5794ca0328656360
Quinn94 <mayquinn94@gmail.com>
2020-10-23 22:00:32 +0800

    routes controller json webtoken

---
+++ backend/controller/productController.js
@@ -0,0 +1,27 @@
+import asyncHandler from 'express-async-handler' //middleware to handle error with Error-Handler
+import Product from '../model/productModel.js'
+
+// @desc  Fetch all products
+// @route GET /api/products
+// @access Public
+const getProducts = asyncHandler(async (req, res) => {
+  const products = await Product.find({});
+  res.json(products);
+})
+
+// @desc  Fetch single product
+// @route GET /api/products/:id
+// @access Public
+const getProductById = asyncHandler(async (req, res) => {
+  const product = await Product.findById(req.params.id)
+
+  if (product) {
+    res.json(product)
+  } else {
+    res.status(404)
+    throw new Error('Product not found')
+    //this will work with the Error-Handler because of express-async-handler
+  }
+})
+
+export { getProducts, getProductById }
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,30 +1,10 @@
 import express from 'express'
-import asyncHandler from 'express-async-handler'
-import Product from '../model/productModel.js'
-
 const router = express.Router()
+import { getProducts, getProductById } from '../controller/productController.js'
 
-// @desc  Fetch all products
-// @route GET /api/products
-// @access Public
-router.get('/', asyncHandler(async (req, res) => {
-  const products = await Product.find({})
-  res.json(products)
-}))
+router.route('/').get(getProducts)
 
-// @desc  Fetch single product
-// @route GET /api/products/:id
-// @access Public
-router.get('/:id', asyncHandler(async (req, res) => {
-  const product = await Product.findById(req.params.id)
+router.route('/:id').get(getProductById)
 
-  if (product) {
-    res.json(product)
-  } else {
-    res.status(404)
-    throw new Error('Product not found') //this will work with the error handler
-  }
-
-}))
 
 export default router
