c8817cbd45ab8ba5df9374482c2b5718ff411cfe
Quinn94 <mayquinn94@gmail.com>
2020-10-24 21:21:29 +0800

    user login and signup

--- backend/model/userModel.js
+++ backend/model/userModel.js
@@ -26,6 +26,16 @@ userSchema.methods.matchPassword = async function (enteredPassword) {
   return await bcrypt.compare(enteredPassword, this.password)
 }
 
+userSchema.pre('save', async function (next) {
+  //don't run this when updating profile, or not a new hash will be created and we won't be able to login
+  if (!this.isModified('password')) {
+    next() //if password is not modified/sent then we want to just move on
+  }
+
+  const salt = await bcrypt.genSalt(10)
+  this.password = await bcrypt.hash(this.password, salt)
+})
+
 const User = mongoose.model('User', userSchema)
 
 export default User
