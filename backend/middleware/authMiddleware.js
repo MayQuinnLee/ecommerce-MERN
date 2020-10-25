f21a26af3cd016823dd057eb66cb8d4b1055b0ed
Quinn94 <mayquinn94@gmail.com>
2020-10-25 15:15:23 +0800

    update user backend

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -11,10 +11,10 @@ const protect = asyncHandler(async (req, res, next) => {
     try {
       token = req.headers.authorization.split(' ')[1]
       const decoded = jwt.verify(token, process.env.JWT_SECRET)
-      console.log(decoded)
+      console.log(`protect - ${decoded}`)
 
       req.user = await User.findById(decoded.id).select('-password')
-      console.log(req.user)
+      console.log(`protect - ${req.user}`)
 
       next()
 
