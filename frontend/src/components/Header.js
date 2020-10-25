4a173cc6016131ddd7c3e6b61467418819cd8924
Quinn94 <mayquinn94@gmail.com>
2020-10-25 22:23:54 +0800

    update user profile

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -35,7 +35,9 @@ const Header = () => {
                   <LinkContainer to='/profile'>
                     <NavDropdown.Item>Profile</NavDropdown.Item>
                   </LinkContainer>
-                  <NavDropdown.Item onClick={logoutHandler}>Logout</NavDropdown.Item>
+                  <LinkContainer to='/login'>
+                    <NavDropdown.Item onClick={logoutHandler}>Logout</NavDropdown.Item>
+                  </LinkContainer>
                 </NavDropdown>
               ) : (
                   <LinkContainer to='/login'>
