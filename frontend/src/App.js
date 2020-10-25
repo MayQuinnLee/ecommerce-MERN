55440988f8f53e6111163aa6edb3ddb5d38327fb
Quinn94 <mayquinn94@gmail.com>
2020-10-25 12:18:18 +0800

    register screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,6 +7,7 @@ import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen.js';
 import CartScreen from './screens/CartScreen.js'
 import LoginScreen from './screens/LoginScreen';
+import RegisterScreen from './screens/RegisterScreen';
 
 const App = () => {
   return (
@@ -17,6 +18,7 @@ const App = () => {
           <Route path='/product/:id' component={ProductScreen} />
           <Route path='/cart/:id?' component={CartScreen} />
           <Route path='/login' component={LoginScreen} />
+          <Route path='/register' component={RegisterScreen} />
           <Route path='/' component={HomeScreen} exact />
         </main>
       </Container>
