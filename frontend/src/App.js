c19efbc2c746376e366771b8e2ce5ed45b0407a5
Quinn94 <mayquinn94@gmail.com>
2020-10-25 18:24:41 +0800

    get user profile

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,6 +8,7 @@ import ProductScreen from './screens/ProductScreen.js';
 import CartScreen from './screens/CartScreen.js'
 import LoginScreen from './screens/LoginScreen';
 import RegisterScreen from './screens/RegisterScreen';
+import ProfileScreen from './screens/ProfileScreen';
 
 const App = () => {
   return (
@@ -19,6 +20,7 @@ const App = () => {
           <Route path='/cart/:id?' component={CartScreen} />
           <Route path='/login' component={LoginScreen} />
           <Route path='/register' component={RegisterScreen} />
+          <Route path='/profile' component={ProfileScreen} />
           <Route path='/' component={HomeScreen} exact />
         </main>
       </Container>
