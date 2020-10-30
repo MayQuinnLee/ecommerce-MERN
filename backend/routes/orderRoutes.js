47c2d81f06a0d0bd9c7456309b117e5cd301c97a
Quinn94 <mayquinn94@gmail.com>
2020-10-30 09:29:25 +0800

    fetch orders to profile page

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,9 +1,10 @@
 import express from 'express'
 const router = express.Router()
 import { protect } from '../middleware/authMiddleware.js'
-import { addOrderItems, getOrderItems, updateOrderToPaid } from '../controller/orderController.js'
+import { addOrderItems, getMyOrders, getOrderItems, updateOrderToPaid } from '../controller/orderController.js'
 
 router.route('/').post(protect, addOrderItems)
+router.route('/myorders').get(protect, getMyOrders)
 router.route('/:id').get(protect, getOrderItems) //make sure that this is at the bottom, or else if you pass in /somethingElse, it will think that it is the id
 router.route('/:id/pay').put(protect, updateOrderToPaid)
 
